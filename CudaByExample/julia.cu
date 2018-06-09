470ae30d1b0e7ea26cc2d4f50b9553fea29396ce
MacroUniverse <270174408@qq.com>
2018-06-08 22:09:52 -0500

    update julia

--- CudaByExample/julia.cu
+++ CudaByExample/julia.cu
@@ -1,20 +1,13 @@
 #include "matsave.h"
 #include "nr3plus.h"
 using namespace std;
-
-// to write mat file
-/* 
-MATFile *pfile = matOpen("julia.mat", "w");
-matsave(x, "x", pfile);
-matClose(pfile);
-*/
-
-#define DIM 4000
+#define H2D cudaMemcpyHostToDevice
+#define D2H cudaMemcpyDeviceToHost
+#define DIM 1000
 
 __device__
-int julia( int x, int y ) {
-    int i;
-    const float scale = 1.5;
+Int julia(Int x, Int y, float scale) {
+    Int i;
     float jx = scale * (float)(DIM/2 - x)/(DIM/2);
     float jy = scale * (float)(DIM/2 - y)/(DIM/2);
     Complex c(-0.8, 0.156);
@@ -29,29 +22,35 @@ int julia( int x, int y ) {
 }
 
 __global__
-void kernel( Uchar *ptr ) {
-    Int x = blockIdx.x;
-    Int y = blockIdx.y;
-    Int juliaValue = julia( x, y );
-    ptr[x + y * gridDim.x] = (Uchar)(255*juliaValue);
+void kernel(Uchar *ptr, float scale) {
+    Int indxy, ind = blockIdx.x*blockDim.x + threadIdx.x;
+    Int stride = blockDim.x*gridDim.x;
+    Int x,y;
+    for(indxy = ind; indxy < DIM*DIM; indxy += stride){
+        x = indxy%DIM; y = indxy/DIM;
+        Int juliaValue = julia( x, y, scale);
+        ptr[x + y * DIM] = (Uchar)(255*juliaValue);
+    }
 }
 
 int main( void ) {
-    int img_size = DIM*DIM*sizeof(Uchar);
+    int i, img_size = DIM*DIM*sizeof(Uchar);
+    float scale = 1.5;
+    string str;
     MatUchar bitmap(DIM, DIM);
     Uchar *dev_bitmap;
 
     cudaMalloc( (void**)&dev_bitmap, img_size );
 
-    dim3 grid(DIM,DIM);
-    kernel<<<grid,1>>>( dev_bitmap );
-
-    cudaMemcpy( bitmap[0], dev_bitmap, img_size,
-                        cudaMemcpyDeviceToHost );
+    for (i = 0; i < 150; ++i){
+        scale *= 0.95;
+        kernel<<<320,32>>>( dev_bitmap, scale );
+        cudaMemcpy(bitmap[0], dev_bitmap, img_size, D2H);
+        str = to_string(i);
+        MATFile *pfile = matOpen((str + ".julia.mat").c_str(), "w");
+        matsave(bitmap, "julia", pfile);
+        matClose(pfile);
+    }
     
-    MATFile *pfile = matOpen("julia.mat", "w");
-    matsave(bitmap, "julia", pfile);
-    matClose(pfile);
     cudaFree( dev_bitmap );
 }
-
