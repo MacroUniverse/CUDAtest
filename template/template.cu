9f05edd2e1ceaa7f028cad397d93b0b6e4951d9d
MacroUniverse <270174408@qq.com>
2018-06-11 07:14:22 +0000

    major update for template project
    all future cuda project should start with template project

--- template/template.cu
+++ template/template.cu
@@ -7,14 +7,27 @@ using std::ifstream; using std::to_string;
 #define D2H cudaMemcpyDeviceToHost
 #define cpySym cudaMemcpyToSymbol
 
-// to write mat file
-/* 
-MATFile *pfile = matOpen("julia.mat", "w");
-matsave(x, "x", pfile);
-matClose(pfile);
-*/
+__global__
+void kernel()
+{
+	printf("In kernel, block = %d, thread = %d\n", blockIdx.x, threadIdx.x);
+}
 
 int main()
 {
-	cout << "hello!" << endl;
+	// cuda kernel call
+	cout << "calling kernel..." << endl;
+	kernel<<<2,2>>>();
+	cudaDeviceSynchronize();
+	cout << "done calling kernel\n" << endl;
+
+	// write data file
+	cout << "writting data file..." << endl;
+	MATFile *pfile = matOpen("nrMat.mat", "w");
+	MatDoub A;
+	A.assign(2, 3, 0.);
+	A[0][0] = 1.; A[0][1] = 3.; A[0][2] = 5.; A[1][2] = 11;
+	matsave(A, "A", pfile);
+	matClose(pfile);
+	cout << "done writing data file" << endl;
 }
